059ef612497beac9e90195d0ea48bac8512ae9b9
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2023-03-07 20:40:37 +0530

    python basic program
    python basic program

---
+++ basics/perimeter.py
@@ -0,0 +1,6 @@
+length=int(input("Enter a length of rectangle"))
+breadth=int(input("Enter a breadth of rectangle"))
+area=length*breadth
+perimeter=2*(length+breadth)
+print(area)
+print(perimeter)
