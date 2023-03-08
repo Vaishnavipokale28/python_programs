794158de93b5d8f0407f9a938dcb13e2cdc96dbe
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2023-03-08 21:48:55 +0530

    python patterns program
    python patterns program

---
+++ for loop/patterns.py
@@ -0,0 +1,40 @@
+for i in range(1,5):
+    for i in range(1,5):
+        print("*",end="")
+    print("")
+
+print(" ")
+
+for i in range(1,5):
+    for j in range(1,i+1):
+        print("*",end="")
+    print("")
+
+
+print("")
+
+for i in range(1,5):
+    for j in range(1,i+1):
+        print(j,end="")
+    print("")
+
+print("")
+
+for i in range(65,69):
+    for j in range(65, i+1):
+        print(chr(j),end=" ")
+    print("")
+
+print("")
+
+for i in range(1,5):
+    for j in range(i,5):
+        print("*",end="")
+    print("")
+
+print("")
+
+for i in range(4,0,-1):
+    for j in range(1,i+1):
+        print(j,end='')
+    print("")
