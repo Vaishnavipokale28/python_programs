c0d7b2c54d4e6558b826852fd72029e10809ce0d
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2023-03-12 22:20:07 +0530

    python if_else program
    check whether alphabet is vowel or consonant

---
+++ if_else/vowel or not.py
@@ -0,0 +1,5 @@
+ch=input("Enter any alphabet")
+if ch=='a' or ch=='e' or ch=='i' or ch=='o' or ch=='u' or ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=="U":
+    print("Entered alphabet is vowel")
+else:
+    print("Entered alphabet is consonant")
