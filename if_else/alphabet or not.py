a543d08f8c1a9f14dc4d21ff13d2265f605fd86d
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2023-03-20 22:48:46 +0530

    if_else program
    check entered character is alphabet or not

---
+++ if_else/alphabet or not.py
@@ -0,0 +1,9 @@
+ch=input("Enter a character")
+
+if ch>='a' and ch<='z':
+    print("character is alphabet")
+elif ch>='A' and ch<='Z':
+    print("character is alphabet")
+else:
+    print("Not alphabet")
+
