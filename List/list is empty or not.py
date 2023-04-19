207ade2849037cb0e22f025fd569060d86f08882
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2023-04-19 20:41:41 +0530

    python list program
    check list is empty or not

---
+++ List/list is empty or not.py
@@ -0,0 +1,13 @@
+list=[]
+if len(list)==0:
+    print("List is empty")
+else:
+    print("List is not empty")
+
+print("------------------------------------")
+
+list=[1,64,74,22,32,2]
+if len(list) == 0:
+    print("List is empty")
+else:
+    print("List is not empty")
