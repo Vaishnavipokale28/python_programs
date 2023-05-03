b0bc144e28c721a62125b00088e6d779f0560031
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2023-05-03 22:18:46 +0530

    python  program
    programs using lambda function

---
+++ lambda function/lambda function.py
@@ -0,0 +1,15 @@
+#addition of two numbers
+add=lambda x,y:x+y
+print(add(29,56))
+
+print("---------------------------")
+
+#area of circle
+area=lambda r:3.14*r*r
+print(area(12))
+
+print("----------------------------")
+
+#square
+sqr=lambda s:s*s
+print(sqr(4))
